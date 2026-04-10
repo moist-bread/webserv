3c5762a441071abe24d864012d3ac3cd8c07630c
Rui Gonçalves <ruigoncalves@Ruis-MacBook-Pro.local>
2026-04-10 17:18:12 +0100

    Parser class -> input handler functions mapped

--- inc/serverConfig/Parser.hpp
+++ inc/serverConfig/Parser.hpp
@@ -3,6 +3,7 @@
 // ==┊ needed libs by class
 #include <iostream>
 #include <vector>
+#include <map>
 #include <stdlib.h>
 
 #include "../ansi_color_codes.h"
@@ -24,19 +25,43 @@ class Parser
 	private:
 		// Parser(void); 				// default constructor
 		Parser(Parser const &source);	// copy constructor
-
 		Parser &operator=(Parser const &source); // copy assignment operator overload
 
 		ServerConfig _parseServerBlock(void);
 		LocationConfig _parseLocationBlock(const std::string &path);
 
+		typedef void (Parser::*ServerHandler)(ServerConfig &);		//	custom type called 'ServerHandler' -> pointer to a Parser method that takes a ServerConfig reference
+		typedef void (Parser::*LocationHandler)(LocationConfig &);	//	custom type called 'LocationHandler' -> pointer to a Parser method that takes a LocationConfig reference
+
+		std::map<std::string, ServerHandler> _serverHandlers;		//	map links the string (e.g., "listen") to the function pointer
+		std::map<std::string, LocationHandler> _locationHandlers;	//	map links the string (e.g., "root") to the function pointer
+
+		//	ServerHandler functions
+		void _serverListen(ServerConfig &server);
+		void _serverName(ServerConfig &server);
+		void _serverMaxBodySize(ServerConfig &server);
+		void _serverErrorPage(ServerConfig &server);
+		void _serverLocation(ServerConfig &server);
+
+		//	LocationHandler functions
+		void _locationRoot(LocationConfig &location);
+		void _locationIndex(LocationConfig &location);
+		void _locationAutoIndex(LocationConfig &location);
+		void _locationAllowMethods(LocationConfig &location);
+		void _locationReturn(LocationConfig &location);
+		void _locationCgi(LocationConfig &location);
+		void _locationUploadStore(LocationConfig &location);
+
 		//	Helpers
 		t_token _expect(e_token_type expected_type);
 		const t_token &_currentToken(void) const;
 		void _advanceToken(void); 
+		void _extractSingleKeyword(std::string &destination);
+		void _extractKeywordVector(std::vector<std::string> &destination);
 
 		size_t	_cursor;
 		const std::vector<t_token> &_tokens;
+
 };
 
 std::ostream &operator<<(std::ostream &out, Parser const &source);
--- src/serverConfig/Parser.cpp
+++ src/serverConfig/Parser.cpp
@@ -5,10 +5,25 @@
 
 Parser::Parser(const std::vector<t_token> &tokens) : _cursor(0), _tokens(tokens)
 {
+	_serverHandlers["listen"] = &Parser::_serverListen;
+	_serverHandlers["server_name"] = &Parser::_serverName;
+	_serverHandlers["client_max_body_size"] = &Parser::_serverMaxBodySize;
+	_serverHandlers["error_page"] = &Parser::_serverErrorPage;
+	_serverHandlers["location"] = &Parser::_serverLocation;
+
+	_locationHandlers["root"] = &Parser::_locationRoot;
+	_locationHandlers["index"] = &Parser::_locationIndex;
+	_locationHandlers["autoindex"] = &Parser::_locationAutoIndex;
+	_locationHandlers["allow_methods"] = &Parser::_locationAllowMethods;
+	_locationHandlers["return"] = &Parser::_locationReturn;
+	_locationHandlers["cgi"] = &Parser::_locationCgi;
+	_locationHandlers["upload_store"] = &Parser::_locationUploadStore;
+
 	std::cout << GRN "the Parser ";
 	std::cout << UCYN "has been created" DEF << std::endl;
 }
 
+// !! Needed?
 // Parser::Parser(void) : _tokens()
 // {
 // 	std::cout << GRN "the Parser ";
@@ -32,7 +47,11 @@ Parser &Parser::operator=(Parser const &source)
 {
 	std::cout << YEL "copy assignment operator overload..." DEF << std::endl;
 	if (this != &source)
-		(void)source;
+	{
+		this->_cursor = source._cursor;
+		this->_serverHandlers = source._serverHandlers;
+		this->_locationHandlers = source._locationHandlers;
+	}
 	return (*this);
 }
 
@@ -67,65 +86,11 @@ ServerConfig Parser::_parseServerBlock(void)
 	while (_currentToken().type != TOKEN_RBRACE)
 	{
 		t_token directiveToken = _expect(TOKEN_KEYWORD);
-		if (directiveToken.content == "listen")
+		std::string directive = directiveToken.content;
+		if (_serverHandlers.count(directive) > 0)
 		{
-			t_token listenToken = _expect(TOKEN_KEYWORD);
-			size_t colonPos = listenToken.content.find(':');
-			if (colonPos != std::string::npos)
-			{
-				newServer.host = listenToken.content.substr(0, colonPos);
-				newServer.port = std::atoi(listenToken.content.substr(colonPos + 1).c_str());
-			}
-			else
-			{
-				newServer.host = "0.0.0.0";
-				newServer.port = std::atoi(listenToken.content.c_str());
-			}
-			_expect(TOKEN_SEMICOLON);
-		}
-		else if (directiveToken.content == "server_name")
-		{
-			while (_currentToken().type == TOKEN_KEYWORD)
-			{
-				newServer.serverNames.push_back(_currentToken().content);
-				_advanceToken();
-			}
-			_expect(TOKEN_SEMICOLON);
-			if (newServer.serverNames.empty())
-				throw std::runtime_error("Syntax error: server_name directive is empty");
-		}
-		else if (directiveToken.content == "client_max_body_size")
-		{
-			t_token sizeToken = _expect(TOKEN_KEYWORD);
-			newServer.clientMaxBodySize = std::strtoul(sizeToken.content.c_str(), NULL, 10);
-			if (*(sizeToken.content.end()) == 'M')
-				newServer.clientMaxBodySize *= 1048576;
-			_expect(TOKEN_SEMICOLON);
-		}
-		else if (directiveToken.content == "error_page")
-		{
-			std::vector<std::string> tempArgs;
-			while (_currentToken().type == TOKEN_KEYWORD)
-			{
-				tempArgs.push_back(_currentToken().content);
-				_advanceToken();
-			}
-			_expect(TOKEN_SEMICOLON);
-			if (tempArgs.size() < 2)
-        		throw std::runtime_error("Syntax error: error_page needs at least one code and a URI");
-			std::string uri = tempArgs.back();
-			tempArgs.pop_back();
-			for (size_t i = 0; i < tempArgs.size(); ++i)
-			{
-				int errorCode = std::atoi(tempArgs[i].c_str());
-				newServer.errorPages[static_cast <t_status_code>(errorCode)] = uri;
-			}
-		}
-		else if (directiveToken.content == "location")
-		{
-			t_token pathToken = _expect(TOKEN_KEYWORD);
-			LocationConfig newLocation = _parseLocationBlock(pathToken.content);
-			newServer.locations.push_back(newLocation);
+			ServerHandler handler = _serverHandlers[directive];
+			(this->*handler)(newServer);
 		}
 		else
 		{
@@ -147,65 +112,18 @@ LocationConfig Parser::_parseLocationBlock(const std::string &path)
 	while (_currentToken().type != TOKEN_RBRACE)
 	{
 		t_token directiveToken = _expect(TOKEN_KEYWORD);
-		if (directiveToken.content == "root")
+		std::string directive = directiveToken.content;
+		if (_locationHandlers.count(directive) > 0)
 		{
-			t_token rootToken = _expect(TOKEN_KEYWORD);
-			newLocation.root = rootToken.content;
-			_expect(TOKEN_SEMICOLON);
-		}
-		else if (directiveToken.content == "index")
-		{
-			t_token indexToken = _expect(TOKEN_KEYWORD);
-			newLocation.index = indexToken.content;
-			_expect(TOKEN_SEMICOLON);
-		}
-		else if (directiveToken.content == "autoindex")
-		{
-			t_token autoindexToken = _expect(TOKEN_KEYWORD);
-			std::string autoindex = autoindexToken.content;
-			if (autoindex != "on" && autoindex != "off")
-				throw std::runtime_error("Syntax error: autoindex must be on/off");
-			newLocation.autoindex = autoindex == "on" ?  true : false;
-			_expect(TOKEN_SEMICOLON);
-		}
-		else if (directiveToken.content == "allow_methods")
-		{
-			while (_currentToken().type == TOKEN_KEYWORD)
-			{
-				t_method method = HTTP::getMethod(_currentToken().content);
-				newLocation.allowedMethods.push_back(method);
-				_advanceToken();
-			}
-			_expect(TOKEN_SEMICOLON);
-			if (newLocation.allowedMethods.empty())
-				throw std::runtime_error("Syntax error: allow_methods directive is empty");
-		}
-		else if (directiveToken.content == "return")
-		{
-			t_token returnToken = _expect(TOKEN_KEYWORD);
-			newLocation.returnCode = std::atoi(returnToken.content.c_str());
-			returnToken = _expect(TOKEN_KEYWORD);
-			newLocation.returnUrl = returnToken.content;
-			_expect(TOKEN_SEMICOLON);
-		}
-		else if (directiveToken.content == "cgi")
-		{
-			t_token extToken = _expect(TOKEN_KEYWORD);
-			t_token execToken = _expect(TOKEN_KEYWORD);
-			newLocation.cgi[extToken.content] = execToken.content;
-			_expect(TOKEN_SEMICOLON);
-		}
-		else if (directiveToken.content == "upload_store")
-		{
-			t_token uploadstoreToken = _expect(TOKEN_KEYWORD);
-			newLocation.uploadStore = uploadstoreToken.content;
-			_expect(TOKEN_SEMICOLON);
+			LocationHandler handler = _locationHandlers[directive];
+			(this->*handler)(newLocation);
 		}
 		else
 		{
 			std::stringstream ss;
 			ss << "Syntax error at line " << directiveToken.line
 				<< ": Unknown locationdirective '" << directiveToken.content << "'";
+			throw std::runtime_error(ss.str());
 		}
 	}
 	_expect(TOKEN_RBRACE);
@@ -214,7 +132,132 @@ LocationConfig Parser::_parseLocationBlock(const std::string &path)
 	return (newLocation);
 }
 
-/*	----- Helpers ----- */
+//*	----- ServerHandler functions ----- */
+
+void Parser::_serverListen(ServerConfig &server)
+{
+	std::string listen;
+	_extractSingleKeyword(listen);
+	size_t colonPos = listen.find(':');
+	if (colonPos != std::string::npos)
+	{
+		server.host = listen.substr(0, colonPos);
+		server.port = std::atoi(listen.substr(colonPos + 1).c_str());
+	}
+	else
+	{
+		server.host = "0.0.0.0";
+		server.port = std::atoi(listen.c_str());
+	}
+	_expect(TOKEN_SEMICOLON);
+}
+
+void Parser::_serverName(ServerConfig &server)
+{
+	_extractKeywordVector(server.serverNames);
+	_expect(TOKEN_SEMICOLON);
+	if (server.serverNames.empty())
+		throw std::runtime_error("Syntax error: server_name directive is empty");
+}
+
+void Parser::_serverMaxBodySize(ServerConfig &server)
+{
+	std::string sizeValue;
+	_extractSingleKeyword(sizeValue);
+	if (!sizeValue.empty() && sizeValue[sizeValue.size() - 1] == 'M')
+		sizeValue.erase(sizeValue.size() - 1);
+	server.clientMaxBodySize = std::strtoul(sizeValue.c_str(), NULL, 10);
+	if (!sizeValue.empty() && sizeValue[sizeValue.size() - 1] == 'M')
+		server.clientMaxBodySize *= 1048576;
+	_expect(TOKEN_SEMICOLON);
+}
+
+void Parser::_serverErrorPage(ServerConfig &server)
+{
+	std::vector<std::string> tempArgs;
+	_extractKeywordVector(tempArgs);
+	_expect(TOKEN_SEMICOLON);
+	if (tempArgs.size() < 2)
+      		throw std::runtime_error("Syntax error: error_page needs at least one code and a URI");
+	std::string uri = tempArgs.back();
+	tempArgs.pop_back();
+	for (size_t i = 0; i < tempArgs.size(); ++i)
+	{
+		int errorCode = std::atoi(tempArgs[i].c_str());
+		server.errorPages[static_cast <t_status_code>(errorCode)] = uri;
+	}
+}
+
+void Parser::_serverLocation(ServerConfig &server)
+{
+	t_token pathToken = _expect(TOKEN_KEYWORD);
+	LocationConfig newLocation = _parseLocationBlock(pathToken.content);
+	server.locations.push_back(newLocation);
+}
+
+//*	----- LocationHandler functions ----- */
+
+void Parser::_locationRoot(LocationConfig &location)
+{
+	_extractSingleKeyword(location.root);
+	_expect(TOKEN_SEMICOLON);
+}
+
+void Parser::_locationIndex(LocationConfig &location)
+{
+	_extractSingleKeyword(location.index);
+	_expect(TOKEN_SEMICOLON);
+}
+
+void Parser::_locationAutoIndex(LocationConfig &location)
+{
+	std::string autoindex;
+	_extractSingleKeyword(autoindex);
+	if (autoindex != "on" && autoindex != "off")
+		throw std::runtime_error("Syntax error: autoindex must be on/off");
+	location.autoindex = autoindex == "on" ?  true : false;
+	_expect(TOKEN_SEMICOLON);
+}
+
+void Parser::_locationAllowMethods(LocationConfig &location)
+{
+	while (_currentToken().type == TOKEN_KEYWORD)
+	{
+		t_method method = HTTP::getMethod(_currentToken().content);
+		location.allowedMethods.push_back(method);
+		_advanceToken();
+	}
+	_expect(TOKEN_SEMICOLON);
+	if (location.allowedMethods.empty())
+		throw std::runtime_error("Syntax error: allow_methods directive is empty");
+}
+
+void Parser::_locationReturn(LocationConfig &location)
+{
+	std::string returnCode;
+	_extractSingleKeyword(returnCode);
+	location.returnCode = std::atoi(returnCode.c_str());
+	_extractSingleKeyword(location.returnUrl);
+	_expect(TOKEN_SEMICOLON);
+}
+
+void Parser::_locationCgi(LocationConfig &location)
+{
+	std::string ext;
+	std::string exec;
+	_extractSingleKeyword(ext);
+	_extractSingleKeyword(exec);
+	location.cgi[ext] = exec;
+	_expect(TOKEN_SEMICOLON);
+}
+
+void Parser::_locationUploadStore(LocationConfig &location)
+{
+	_extractSingleKeyword(location.uploadStore);
+	_expect(TOKEN_SEMICOLON);
+}
+
+//*	----- Helpers ----- */
 
 t_token Parser::_expect(e_token_type expected_type)
 {
@@ -234,7 +277,7 @@ t_token Parser::_expect(e_token_type expected_type)
 
 const t_token &Parser::_currentToken(void) const
 {
-	if (this->_cursor > this->_tokens.size())
+	if (this->_cursor >= this->_tokens.size())
 		throw std::runtime_error("Parser error: Unexpected end of tokens");
 	return (this->_tokens[this->_cursor]);
 }
@@ -243,4 +286,19 @@ void Parser::_advanceToken(void)
 {
 	if (this->_cursor < this->_tokens.size())
 		this->_cursor++;
+}
+
+void Parser::_extractSingleKeyword(std::string &destination)
+{
+	t_token token = _expect(TOKEN_KEYWORD);
+	destination = token.content;
+}
+
+void Parser::_extractKeywordVector(std::vector<std::string> &destination)
+{
+	while (_currentToken().type == TOKEN_KEYWORD)
+	{
+		destination.push_back(_currentToken().content);
+		_advanceToken();
+	}
 }
